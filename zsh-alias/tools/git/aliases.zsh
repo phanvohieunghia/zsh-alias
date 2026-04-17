d7cb56f564b0d50a15598cfc01196ea0b171731a
Noah <phanvohieunghia@gmail.com>
2026-04-17 17:38:12 +0700

    fix: path for git alias

--- zsh-alias/tools/git/aliases.zsh
+++ zsh-alias/tools/git/aliases.zsh
@@ -76,3 +76,70 @@ gwip() {
   git add -A
   git commit -m "wip: $(date '+%Y-%m-%d %H:%M')"
 }
+
+# # git
+# alias gs='g status -s;'
+# alias ga='g add .;'
+# alias gcm='g commit -m'
+# # git push
+# alias gp='g push;'
+# alias gpf='g push --force-with-lease;'
+# # git pull
+# alias gl='g pull;'
+# alias glf='g pull --force;'
+# # git switch
+# alias gwst='g switch staging;'
+# alias gwprod='g switch production;'
+# alias gw='g switch'
+# alias gwc='g switch -c'
+# alias gwp='git switch $(git branch -r | fzf | sed "s/origin\///")'
+# # git stash
+# alias gss='g stash save -u'
+# alias gsp='g stash pop;'
+# alias gsd='g stash drop;'
+# alias gsa='g stash apply;'
+# # git another alias
+# alias grc='g rebase --continue;'
+# alias gra='g rebase --abort;'
+# alias grs='g rebase --skip;'
+# alias gr='g rebase'
+# alias gri='g rebase -i'
+# alias openpr='open -a "Google Chrome" "$(git remote get-url origin | sed '"'"'s|git@\(.*\):\(.*\)|https://\1/\2|'"'"' | sed '"'"'s|\.git$||'"'"')/compare/qc...$(git branch --show-current)"'
+# # Custom alias
+# alias ngr='g switch staging ; g pull ; g switch - ; g rebase staging;'
+# alias nf='pbpaste | sed -E "s/[[:space:]]+/-/g" | tr "[:upper:]" "[:lower:]" | pbcopy;'
+# alias nyr='cp .env.local_ .env &&  npx yarn dev;'
+# alias nyrd='cp .env.development_ .env && npx yarn dev;'
+# alias cl='clear;'
+# alias nscan='next dev & npx react-scan@latest localhost:3000'
+# alias cb="g branch --show-current | tr -d '\n' | pbcopy;"
+# alias kiro="open -a Kiro"
+# alias gpr='function _gpr() {
+#   local pr_number=$1
+#   local remote_url=$(git remote get-url origin 2>/dev/null)
+
+#   if [ -z "$remote_url" ]; then
+#     echo "❌ Không tìm thấy git remote origin"
+#     return 1
+#   fi
+
+#   if [ -z "$pr_number" ]; then
+#     echo "❌ Thiếu số PR. Dùng: gpr <số_pr>"
+#     return 1
+#   fi
+
+#   # Chuẩn hóa URL (SSH → HTTPS)
+#   remote_url=$(echo "$remote_url" | sed "s/git@github.com:/https:\/\/github.com\//; s/git@gitlab.com:/https:\/\/gitlab.com\//; s/\.git$//")
+
+#   # Phân biệt GitHub vs GitLab
+#   if echo "$remote_url" | grep -q "github.com"; then
+#     open "${remote_url}/pull/${pr_number}"
+#   elif echo "$remote_url" | grep -q "gitlab.com"; then
+#     open "${remote_url}/-/merge_requests/${pr_number}"
+#   else
+#     echo "❌ Không nhận diện được host (chỉ hỗ trợ GitHub/GitLab)"
+#     return 1
+#   fi
+# }; _gpr'
+
+
